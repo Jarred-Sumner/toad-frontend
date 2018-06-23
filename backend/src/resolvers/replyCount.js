85802083d09860e1588d56843cfb85d886533aaa
Luke Miles <luke@lukemil.es>
2018-06-23 10:40:27 -0700

    Fix issue with reply count

--- backend/src/resolvers/replyCount.js
+++ backend/src/resolvers/replyCount.js
@@ -3,5 +3,5 @@ import Models from '../models'
 export default thread => {
   const board = thread.board || thread._modelOptions.name.singular
   const threadId = thread.id
-  return Models.db.models[board].count({ where: { parent: threadId } })
+  return Models[board].count({ where: { parent: threadId } })
 }
