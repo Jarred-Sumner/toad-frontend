e03b629bba39b97a58d315e16f90af6043c56426
Luke Miles <luke@lukemil.es>
2018-06-24 12:34:34 -0700

    Use env var for prod pg

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,7 +2,6 @@ import deo from 'deo'
 
 // make sure to set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY for aws
 export default deo({
-  pg_url: 'postgresql://toads@localhost/toads',
   origin: 'localhost:5000',
   hostname: 'http://localhost:5000',
 })
