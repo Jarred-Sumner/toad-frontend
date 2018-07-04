aed1505a3fceec6e74dcb9119818dd74fcf77185
Luke Miles <luke@lukemil.es>
2018-07-03 21:50:33 -0700

    Better name gen

--- backend/src/utils/namegen.js
+++ backend/src/utils/namegen.js
@@ -1,9 +1,25 @@
-import faker from 'faker'
+import Chance from 'chance'
+
+import { sample, random, times } from 'lodash'
 import { random as randomEmoji } from 'node-emoji'
+
+const chance = new Chance()
+
+const genders = ['male', 'female']
+const getGender = () => sample(genders)
 
 const randomColonEmoji = () => `:${randomEmoji().key}:`
 
-export default () =>
-  faker.fake(
-    `{{name.firstName}} {{name.lastName}}, {{name.suffix}} ${randomColonEmoji()}${randomColonEmoji()}`
-  )
+const genName = () => {
+  const gender = getGender()
+  const showSuffix = random(1, 5) === 1
+  const nationality = random(1, 10) === 1 ? 'it' : 'en'
+  const middleInitial = random(1, 100) >= 98
+  return `${chance.name({
+    gender,
+    suffix: showSuffix,
+    middle_initial: middleInitial,
+    nationality,
+  })} ${randomColonEmoji()}${randomColonEmoji()}`
+}
+export default genName
